a2a6127cc0568150dc70288fd404efe2a5a2436e
Tufail <tufail_syed@outlook.com>
2020-08-20 03:50:12 +0100

    Can now add and edit cvs

--- cv/urls.py
+++ cv/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('cv/', views.cv, name='cv'),
+    path('cv/new/', views.cv_new, name='cv_new'),
+    path('cv/<int:pk>/edit/', views.cv_edit, name='cv_edit'),
 ]
--- cv/views.py
+++ cv/views.py
@@ -2,11 +2,33 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.shortcuts import redirect
 from .models import CV, TestModel
+from .forms import CVForm
 
 def cv(request):
-    if request.method == 'POST':
-        TestModel.objects.create(text=request.POST['TestModel_text'])
-        return redirect('/cv/')
-        
-    TestModels = TestModel.objects.all()
-    return render(request, 'cv/cv.html', {'TestModels' : TestModels})
+    cvs = CV.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'cv/cv.html', {'cvs' : cvs})
+
+def cv_new(request):
+    if request.method == "POST":
+        form = CVForm(request.POST)
+        if form.is_valid():
+            cv = form.save(commit=False)
+            cv.published_date = timezone.now()
+            cv.save()
+            return redirect('cv_edit', pk=cv.pk)
+    else:
+        form = CVForm()
+    return render(request, 'cv/cv_edit.html', {'form': form})
+
+def cv_edit(request, pk):
+    cv = get_object_or_404(CV, pk=pk)
+    if request.method == "POST":
+        form = CVForm(request.POST, instance=cv)
+        if form.is_valid():
+            cv = form.save(commit=False)
+            cv.published_date = timezone.now()
+            cv.save()
+            return redirect('cv')
+    else:
+        form = CVForm(instance=cv)
+    return render(request, 'cv/cv_edit.html', {'form': form})
