bfe0cafe8e76766bd38c714ddd722c16282caf44
Tufail <tufail_syed@outlook.com>
2020-08-24 05:47:56 +0100

    Functional tests are complete

--- blog/tests.py
+++ blog/tests.py
@@ -3,24 +3,25 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
-from blog.views import post_list, post_detail
+from blog.views import post_list, post_detail, post_edit, post_draft_list
 from .models import Post
 from .forms import PostForm
 
-class PostListTest(TestCase):
-
-    def test_root_url_resolves_to_post_list_view(self):
-        found = resolve('/')
-        print(found.url_name)  
-        self.assertEqual(found.func, post_list)  
-
-    def test_post_list_returns_correct_html(self):
+class blog_urls_tests(TestCase):
+ 
+    def test_post_list_uses_correct_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'blog/post_list.html')
 
-class PostDetailTest(TestCase):
-
-    def test_root_url_resolves_to_post_detail_view(self):
+    def test_post_detail_url_resolves_to_post_detail_view(self):
         banter = resolve('/post/1/')
-        print(banter.url_name)  
+        #print(banter.url_name)  
         self.assertEqual(banter.func, post_detail)
+
+    def test_post_new_uses_correct_template(self):
+        response = resolve('/post/1/edit')
+        self.assertEqual(response.func, post_edit)
+
+    def test_drafts_uses_correct_template(self):
+        response = resolve('/drafts/')
+        self.assertEqual(response.func, post_draft_list)
--- cv/functional_tests.py
+++ cv/functional_tests.py
@@ -32,11 +32,27 @@ class NewVisitorTest(unittest.TestCase):
         inputbox_password = self.browser.find_element_by_id('id_password').send_keys('TestUser')
         click_login = self.browser.find_element_by_id('login_button').click()
         
-        time.sleep(0.5)
+        #time.sleep(0.5)
         
         find_user = self.browser.find_element_by_css_selector('p.top-menu').text
         #print(find_user)
         self.assertIn('Hello TestUser (Log out)', find_user)
+
+    def test_can_sign_out(self):
+        self.browser.get('http://127.0.0.1:8000/')
+        find_login = self.browser.find_element_by_xpath("//a[@href='/accounts/login/']").click()
+        
+        inputbox_username = self.browser.find_element_by_id('id_username').send_keys('TestUser')
+        inputbox_password = self.browser.find_element_by_id('id_password').send_keys('TestUser')
+        click_login = self.browser.find_element_by_id('login_button').click()
+        signed_in_menu = self.browser.find_element_by_class_name('top-menu')
+
+        find_logout = self.browser.find_element_by_xpath("//a[@href='/accounts/logout/']").click()
+        
+        find_login_button = self.browser.find_element_by_xpath("//a[@href='/accounts/login/']").text
+        signed_out_menu = self.browser.find_element_by_class_name('top-menu')
+
+        self.assertNotEqual(signed_in_menu, signed_out_menu)
 
     def test_can_add_a_comment(self):
         self.browser.get('http://127.0.0.1:8000/')
@@ -47,13 +63,37 @@ class NewVisitorTest(unittest.TestCase):
         inputbox_text = self.browser.find_element_by_id('id_text').send_keys('Pls Work')
         find_send_button = self.browser.find_element_by_id('send_button').click()
         
-        time.sleep(0.5)
+        #time.sleep(0.5)
         
         find_comment_author = self.browser.find_element_by_class_name('comment_author').text
         find_comment_text = self.browser.find_element_by_css_selector('.comment').text
         #print(find_comment_text)
         self.assertIn('Tufail', find_comment_author)
         self.assertIn('Pls Work', find_comment_text)
+
+    def test_can_make_a_blog_post(self):
+        self.browser.get('http://127.0.0.1:8000/')
+        find_login = self.browser.find_element_by_xpath("//a[@href='/accounts/login/']").click()
+        
+        inputbox_username = self.browser.find_element_by_id('id_username').send_keys('TestUser')
+        inputbox_password = self.browser.find_element_by_id('id_password').send_keys('TestUser')
+        click_login = self.browser.find_element_by_id('login_button').click()
+
+        find_new_blog_button = self.browser.find_element_by_xpath("//a[@href='/post/new/']").click()
+
+        inputbox_title = self.browser.find_element_by_id('id_title').send_keys('Test Blog')
+        inputbox_text = self.browser.find_element_by_id('id_text').send_keys('This actually works')
+        find_save_button = self.browser.find_element_by_id('save_button').click()
+
+        find_publish_button = self.browser.find_element_by_id('publish_button').click()
+
+        find_post_title = self.browser.find_element_by_id('post_title').text
+        find_post_description = self.browser.find_element_by_id('post_description').text
+        find_post_text = self.browser.find_element_by_id('post_text').text
+
+        self.assertIn('Test Blog', find_post_title)
+        self.assertIn('Enter description of post here', find_post_description)
+        self.assertIn('This actually works', find_post_text)
 
     def test_can_get_to_cv_from_blog(self):
         self.browser.get('http://127.0.0.1:8000/')
